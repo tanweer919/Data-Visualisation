3b12c044280a5c944b6cf39717703c990720f506
tanweer anwar <tanweer.anwar919@gmail.com>
2019-06-23 09:47:51 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.sendFile('dashboard.html')
+})
+
+app.get('/csv', (req, res) => {
+  res.sendFile('accident.json', {root: __dirname});
+});
+app.get('/visualization', (req, res) => {
+  res.render('visualization');
+});
+
+app.listen(4000, () => {
+  console.log("server started");
+});
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use('view engine', 'ejs')
