adc25558be03919d4ef4c7e6003d7fbbf855eee5
tanweer anwar <tanweer.anwar919@gmail.com>
2019-06-23 09:54:24 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => {
-  res.sendFile('dashboard.html')
-})
+app.use('view engine', 'ejs')
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/csv', (req, res) => {
   res.sendFile('accident.json', {root: __dirname});
@@ -10,13 +13,6 @@ app.get('/csv', (req, res) => {
 app.get('/visualization', (req, res) => {
   res.render('visualization');
 });
-
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT||4000, () => {
   console.log("server started");
 });
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.use('view engine', 'ejs')
