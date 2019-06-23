e52fd721eaa0e22690cc3556299970b495475340
tanweer anwar <tanweer.anwar919@gmail.com>
2019-06-23 10:10:58 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-app.use('view engine', 'ejs')
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+"/public"))
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
